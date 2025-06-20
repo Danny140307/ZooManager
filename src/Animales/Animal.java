e1d5f534785d6a3cc0662c31c71d1659ea5f2fb9
Danny <dachavesva@est.utn.ac.cr>
2025-06-20 14:39:30 -0600

    Actualizacion Clase Animal
    Se termino de hacer la clase animal con sus respectivas validaciones en la fecha de nacimiento y la validacion del Id Del animal

--- src/Animales/Animal.java
+++ src/Animales/Animal.java
@@ -4,10 +4,66 @@
  */
 package Animales;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 /**
  *
  * @author Student
  */
 public class Animal {
-    
+
+    private String idAnimal;
+    private String nombre;
+    private String especie;
+    private LocalDateTime fechas;
+    private zonasZoo zonas;
+
+    public String getIdAnimal() {
+        return idAnimal;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public LocalDateTime getFechas() {
+        return fechas;
+    }
+
+    public zonasZoo getZona() {
+        return zonas;
+    }
+
+    public void setFechas(LocalDateTime fechas) {
+        this.fechas = fechas;
+    }
+
+    public void setZonas(zonasZoo zonas) {
+        this.zonas = zonas;
+    }
+
+    public Animal(String idAnimal, String nombre, String especie, LocalDateTime fechas, String zona) {
+
+        if (idAnimal != null && idAnimal.matches("^\\w{1}-\\d{4}$")) {
+            this.idAnimal = idAnimal;
+        }
+        this.nombre = nombre;
+        this.especie = especie;
+        
+        this.zonas = zonasZoo.ConservacionYRescate;
+        if (fechas != null && fechas.isAfter(LocalDateTime.now())){
+            this.fechas = fechas;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" + "idAnimal=" + idAnimal + ", nombre=" + nombre + ", especie=" + especie + ", fechas=" + fechas + ", zonas=" + zonas + '}';
+    }
+
 }
--- src/Animales/zonasZoo.java
+++ src/Animales/zonasZoo.java
@@ -8,6 +8,29 @@ package Animales;
  *
  * @author Student
  */
-public class zonasZoo {
+public enum zonasZoo {
+    Graja("Granja"),
+    Contacto("Contacto"),
+    Alimentacion("Alimentacion"),
+    ConservacionYRescate("Conservacion y Rescate"),
+    TunelNocturno("Tunel Nocturno"),
+    BiodivesidadLocal("Biodiversidad Local");
+    
+    private final String zonas;
+
+    zonasZoo(String zonas){
+        this.zonas = zonas;
+    }
+    
+    public String getZonas() {
+        return zonas;
+    }
+
+    @Override
+    public String toString() {
+        return "zonasZoo{" + "zonas=" + zonas + '}';
+    }
+    
+    
     
 }
